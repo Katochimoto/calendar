0131817956fcd2692280d326b4fc2bb26917cec3
Katochimoto <bufpost@yandex.ru>
2017-02-15 21:57:54 +0300

    сетка времени

--- src/components/Calendar/components/DayHours/index.js
+++ src/components/Calendar/components/DayHours/index.js
@@ -37,28 +37,57 @@ export default class DayHours extends Component {
       const hour = hoursOfDay[ i ];
 
       elements.push(
-        <div key={hour} className={styles.calendar_DayHours_Item}>
-          <div className={styles.calendar_DayHours_Item_Content} data-hour={hours[ hour ].title} />
-        </div>
+        <div key={hour}
+          className={styles.calendar_DayHours_Item}
+          data-hour={hours[ hour ].title} />
       );
     }
 
     const hour = hoursOfDay[ len ];
 
     elements.push(
-      <div key={hour} className={styles.calendar_DayHours_Item}>
-        <div className={styles.calendar_DayHours_Item_Content}
-          data-hour={hours[ hour ].title}
-          data-next-hour={hours[ hoursOfDay[0] ].title} />
-      </div>
+      <div key={hour}
+        className={styles.calendar_DayHours_Item}
+        data-hour={hours[ hour ].title}
+        data-next-hour={hours[ hoursOfDay[0] ].title} />
     );
 
     return (
-      <div className={styles.calendar_DayHours2}>
-        <div className={styles.calendar_DayHours}>
-          {elements}
-        </div>
+      <div className={styles.calendar_DayHours}>
+        {elements}
       </div>
     );
   }
 }
+
+/*
+for (let i = 0; i < len; i++) {
+  const hour = hoursOfDay[ i ];
+
+  elements.push(
+    <tr key={hour} className={styles.calendar_DayHours_Item}>
+      <td className={styles.calendar_DayHours_Content}>
+        <span className={styles.calendar_DayHours_Current}>{hours[ hour ].title}</span>
+      </td>
+    </tr>
+  );
+}
+
+const hour = hoursOfDay[ len ];
+
+elements.push(
+  <tr key={hour} className={styles.calendar_DayHours_Item}>
+    <td className={styles.calendar_DayHours_Content}>
+      <span className={styles.calendar_DayHours_Current}>{hours[ hour ].title}</span>
+    </td>
+  </tr>
+);
+
+return (
+  <table className={styles.calendar_DayHours}>
+    <tbody>
+      {elements}
+    </tbody>
+  </table>
+);
+ */

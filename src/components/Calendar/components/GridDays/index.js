bfb2b19287c5b1c1d94848a5c888cf38153ee638
Katochimoto <bufpost@yandex.ru>
2017-02-15 16:02:56 +0300

    фикс сетки в Opera 12

--- src/components/Calendar/components/GridDays/index.js
+++ src/components/Calendar/components/GridDays/index.js
@@ -21,16 +21,20 @@ export default class GridDays extends Component {
   render () {
     return (
       <table className={styles.calendar_GridDays}>
-        <tr className={styles.calendar_GridDays_Tr1}>
-          <td className={styles.calendar_GridDays_Td1}>
-            <GridDaysHeader />
-          </td>
-        </tr>
-        <tr className={styles.calendar_GridDays_Tr2}>
-          <td className={styles.calendar_GridDays_Td2}>
-            <GridDaysContent ref={contentComponent => this._contentComponent = contentComponent} />
-          </td>
-        </tr>
+        <thead>
+          <tr>
+            <td>
+              <GridDaysHeader />
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={styles.calendar_GridDays_Content}>
+              <GridDaysContent ref={contentComponent => this._contentComponent = contentComponent} />
+            </td>
+          </tr>
+        </tbody>
       </table>
     );
   }

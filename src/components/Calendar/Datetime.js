ce008cc493db70e70914e6359efa36df8f082b72
Katochimoto <bufpost@yandex.ru>
2017-02-20 22:28:48 +0300

    форматированный вывод дня недели в сетке дня

--- src/components/Calendar/Datetime.js
+++ src/components/Calendar/Datetime.js
@@ -1,28 +1,21 @@
 import fecha from 'fecha';
 
-const PARSE_CACHE = {};
-
 export default function Datetime (strategy) {
 
 }
 
 Datetime.prototype = {
-  parse (sDate, format) {
-    const key = `${sDate}>${format}`;
-    return PARSE_CACHE[ key ] || (PARSE_CACHE[ key ] = fecha.parse(sDate, format));
-  },
-
   gridDaysHourTitle (hour) {
     return String(hour);
   },
 
   gridDaysDayTitle (sDate) {
-    const date = this.parse(sDate, 'YYYY-MM-DD');
+    const date = fecha.parse(sDate, 'YYYY-MM-DD');
     return fecha.format(date, 'ddd, D');
   },
 
   offsetDay (sDate, offset) {
-    const date = this.parse(sDate, 'YYYY-MM-DD');
+    const date = fecha.parse(sDate, 'YYYY-MM-DD');
     date.setUTCDate(date.getUTCDate() + offset);
     return fecha.format(date, 'YYYY-MM-DD');
   }

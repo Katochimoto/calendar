69052bfedbaed008bd84b3ca35dfb38530152940
Katochimoto <bufpost@yandex.ru>
2017-07-28 16:41:27 +0300

    aside

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,9 +4,18 @@ import styles from './index.less';
 
 const Navbar = () => (
   <nav className={styles.Navbar}>
-    <Link className={`${styles.Navbar_Tab} ${styles.Navbar_Tab__active}`} to="/">Main</Link>
-    <Link className={styles.Navbar_Tab} to="/about">About</Link>
-    <Link className={styles.Navbar_Tab} to="/topics">Topics</Link>
+    <Link className={`${styles.Navbar_Tab} ${styles.Navbar_Tab__active}`} to="/">
+      Неделя
+    </Link>
+    <Link className={styles.Navbar_Tab} to="/about">
+      Месяц
+    </Link>
+    <Link className={styles.Navbar_Tab} to="/topics">
+      Год
+    </Link>
+    <Link className={styles.Navbar_Tab} to="/topics">
+      Повестка
+    </Link>
   </nav>
 );
 

ac5d7e6d86d571480949abc78080530bf2d5cdbf
Katochimoto <bufpost@yandex.ru>
2017-07-31 10:50:59 +0300

    dav

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom';
 import { MemoryRouter as Router } from 'react-router-dom';
 import App from './components/App';
 
+// import dav from 'dav';
+
 // https://reacttraining.com/react-router/
 
 ReactDOM.render((
@@ -12,3 +14,15 @@ ReactDOM.render((
     <App />
   </Router>
 ), document.body.appendChild(document.createElement('div')));
+
+// var client = new dav.Client(xhr);
+// client.createAccount({
+//   server: 'http://dav.example.com',
+//   accountType: 'carddav'
+// })
+// .then(function(account) {
+//   account.addressBooks.forEach(function(addressBook) {
+//     console.log('Found address book name ' + addressBook.displayName);
+//     // etc.
+//   });
+// });

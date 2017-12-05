7dafdbd04c9e0a7a1b09519b4289a94913d22dc4
Katochimoto <bufpost@yandex.ru>
2017-12-05 21:06:04 +0300

    remove calendar grid component

--- src/components/Calendar/context.js
+++
@@ -1,4 +0,0 @@
-export default (function () {
-  /* eslint no-eval: 0 */
-  return this || (1, eval)('this');
-})();
--- src/components/Calendar/utils/decorators/mixin.js
+++
@@ -1,6 +0,0 @@
-export default function mixin (mixins = []) {
-  return function (component) {
-    const proto = component.prototype;
-    mixins.forEach(item => Object.assign(proto, item));
-  };
-}
